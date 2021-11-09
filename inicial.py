15a599cad1b45a406faddd721298b66a46c567fa
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2021-11-08 23:26:25 -0300

    Exemplo

---
+++ inicial.py
@@ -0,0 +1,16 @@
+import time
+import os
+import pandas as pd
+
+tempo_inicial = time.time()
+
+arquivos  = os.listdir()
+
+tabela_final = pd.DataFrame()
+for arquivo in arquivos:
+    if "xlsx" in arquivo:
+        tabela = pd.read_excel(arquivo)
+        faturamento = tabela["Valor Final"].sum()
+        print(f"Faturamento da Loja {arquivo.replace('.xlsx', '')} foi de R${faturamento:,.2f}")
+
+print(f"Demorou: {time.time() - tempo_inicial}")
